36d3959998172d45586c5d770e0327ad4cd1268d
Yajaira <sheyla.chuco@tecsup.edu.pe>
2026-08-26 10:12:06 -0500

    Commit 4: Implementación del calendario de pagos

--- Semana01/Actividad02.playground/Contents.swift
+++ Semana01/Actividad02.playground/Contents.swift
@@ -76,3 +76,24 @@ print("Monto de compra: S/ \(montoCompra)")
 print("Interés: S/ \(interes)")
 print("Monto final: S/ \(montoFinal)")
 print("Cuota mensual: S/ \(cuotaMensual)")
+
+
+print("")
+print("===== Tu CALENDARIO DE PAGOS =====")
+
+print("Mes\tMonto Inicial\tCuota Mensual\tResta x Pago")
+
+var montoPendiente = montoFinal
+
+for mes in 1...meses {
+    
+    let montoInicial = montoPendiente
+    
+    montoPendiente = montoPendiente - cuotaMensual
+    
+    if montoPendiente < 0 {
+        montoPendiente = 0
+    }
+    
+    print("\(mes)\tS/ \(montoInicial)\tS/ \(cuotaMensual)\tS/ \(montoPendiente)")
+}
