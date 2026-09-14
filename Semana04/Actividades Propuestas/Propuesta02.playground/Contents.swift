57c5af1d8671172179a12bc827fc88dd8760db99
Sheyla Chuco <sheyla.chuco@tecsup.edu.pe>
2026-09-14 17:44:21 -0500

    feat: Actividad 02 - subclases ClienteNatural y ClienteJuridico

--- Semana04/Actividades Propuestas/Propuesta02.playground/Contents.swift
+++ Semana04/Actividades Propuestas/Propuesta02.playground/Contents.swift
@@ -24,3 +24,41 @@ class Cliente {
         print("Monto mínimo de apertura: S/ \(String(format: "%.2f", montoMinimoApertura))")
     }
 }
+class ClienteNatural: Cliente {
+    let nombreCompleto: String
+    let dni: String
+
+    init(nombreCompleto: String, dni: String, codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double) {
+        self.nombreCompleto = nombreCompleto
+        self.dni = dni
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro, numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("Cliente Natural:")
+        print("Nombre: \(nombreCompleto)")
+        print("DNI: \(dni)")
+        super.mostrarDatos()
+    }
+}
+
+class ClienteJuridico: Cliente {
+    let razonSocial: String
+    let ruc: String
+    let representanteLegal: String
+
+    init(razonSocial: String, ruc: String, representanteLegal: String, codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double) {
+        self.razonSocial = razonSocial
+        self.ruc = ruc
+        self.representanteLegal = representanteLegal
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro, numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("Cliente Jurídico:")
+        print("Razón Social: \(razonSocial)")
+        print("RUC: \(ruc)")
+        print("Representante Legal: \(representanteLegal)")
+        super.mostrarDatos()
+    }
+}
