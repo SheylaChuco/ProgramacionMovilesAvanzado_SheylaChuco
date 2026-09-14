d4569ccfffe3dcd8a550d03db00043b80d874edc
Sheyla Chuco <sheyla.chuco@tecsup.edu.pe>
2026-09-14 17:45:07 -0500

    feat: Actividad 02 - simulacion clientes natural y juridico

--- Semana04/Actividades Propuestas/Propuesta02.playground/Contents.swift
+++ Semana04/Actividades Propuestas/Propuesta02.playground/Contents.swift
@@ -62,3 +62,11 @@ class ClienteJuridico: Cliente {
         super.mostrarDatos()
     }
 }
+// --- Simulación ---
+let clienteNatural = ClienteNatural(nombreCompleto: "Juan Pérez", dni: "12345678", codigo: "C001", direccion: "Av. Lima 123", fechaDeRegistro: "2025-04-03", numeroCuenta: "001-2025-000123", montoMinimoApertura: 500.00)
+
+let clienteJuridico = ClienteJuridico(razonSocial: "Soluciones SAC", ruc: "20123456789", representanteLegal: "María León", codigo: "C002", direccion: "Jr. Empresas 456", fechaDeRegistro: "2025-04-01", numeroCuenta: "001-2025-000456", montoMinimoApertura: 3000.00)
+
+clienteNatural.mostrarDatos()
+print("------------------------")
+clienteJuridico.mostrarDatos()
